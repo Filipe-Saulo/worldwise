715ec1435e9e163e1335dbe09b1557d35227033f
Filipe <filipeunithing@gmail.com>
2024-10-04 14:17:33 -0300

    criando lista de paises

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,29 @@
+import Message from "./Message";
+import Spinner from "./Spinner";
+import styles from "./CountriesList.module.css";
+import CountryItem from "./CountryItem";
+
+function CountriesList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add sua primeira cidade clicando no mapa" />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else {
+      return arr;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
